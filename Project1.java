07f05adf6e9c823c1402866376dce0503ff987a5
Shoegur <54241092+Shoegur@users.noreply.github.com>
2019-09-05 21:15:35 +0530

    Create Project1.java

---
+++ Project1.java
@@ -0,0 +1,20 @@
+
+package swapping;
+
+public class Swapping 
+{
+    public static void main(String[] args) 
+    
+    {
+      int x,y,temp;
+      x= 15;
+      y = 89;
+      System.out.println("Before Swapping : x,y = "+x+", "+ + y);
+      temp = x;
+      x=y;
+      y = temp;
+      System.out.println("After Swapping: x,y = "+x+", "+ + y);
+      
+    }
+    
+}
